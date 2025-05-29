80432bf99987272ce2d461b1a953a6c3251df9f1
daang04 <117044840+daang04@users.noreply.github.com>
2025-05-28 23:09:59 -0500

    asdsad

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.title("Autenticación")
 
 if st.button("Autenticación"):
-    st.login("google")
+    st.login()
 
 st.json(st.experimental_user)
-st.write(st.user)
+st.write(f"Hello, {st.user.name}!")
 
